00c0bc15135011b0694eed7d746abbb021579417
rdrgn <noreply@remin.jp>
2018-08-14 03:30:09 +0900

    add rim lighting of MToon

--- src/shaders/mtoon_vert.glsl
+++ src/shaders/mtoon_vert.glsl
@@ -1,6 +1,8 @@
+varying vec3 vNormal;
 varying vec2 vUv;
 
 void main() {
+  vNormal = (modelMatrix * vec4(normal, 1.0)).xyz;
   vUv = uv;
   vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
   gl_Position = projectionMatrix * mvPosition;
