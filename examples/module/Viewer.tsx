1c2468ae4aa8195e936e49f7fd98e85ed3ebd48a
rdrgn <noreply@remin.jp>
2019-03-11 02:39:27 +0900

    update example for physics

--- examples/module/Viewer.tsx
+++ examples/module/Viewer.tsx
@@ -3,7 +3,7 @@ import DatGui, { DatBoolean, DatButton, DatColor, DatFolder, DatNumber } from 'r
 import 'react-dat-gui/build/react-dat-gui.css';
 import * as THREE from 'three';
 import OrbitControls from 'three-orbitcontrols';
-import { VRM, VRMAnimationClip, VRMAnimationMixer, VRMVMD } from '../../src';
+import { VRM, VRMAnimationClip, VRMAnimationMixer, VRMPhysics, VRMVMD } from '../../src';
 
 interface Props {
   vrm?: VRM;
@@ -32,6 +32,7 @@ export default class Viewer extends React.Component<Props, State> {
   private clip: VRMAnimationClip;
   private mixer: VRMAnimationMixer;
   private actions: THREE.AnimationAction[];
+  private physics: VRMPhysics;
 
   constructor(props: Props) {
     super(props);
@@ -39,13 +40,27 @@ export default class Viewer extends React.Component<Props, State> {
       isInitialized: false,
       isBusy: false,
       progress: 0,
-      data: { background: '#212121', isAxesVisible: true, timeScale: 1 },
+      data: {
+        background: '#212121',
+        isAxesVisible: true,
+        timeScale: 1,
+        positionX: 0,
+        positionY: 0,
+        positionZ: 0,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0,
+        scaleX: 1,
+        scaleY: 1,
+        scaleZ: 1,
+      },
     };
 
     this.clock = new THREE.Clock();
 
     this.onDataUpdate = this.onDataUpdate.bind(this);
     this.update = this.update.bind(this);
+    this.resetTransform = this.resetTransform.bind(this);
     this.restartAnimation = this.restartAnimation.bind(this);
   }
 
@@ -58,7 +73,7 @@ export default class Viewer extends React.Component<Props, State> {
     window.cancelAnimationFrame(this.requestID);
   }
 
-  public componentDidUpdate(prevProps: Props) {
+  public componentDidUpdate(prevProps: Props, prevState: State) {
     // On screen resized.
     if (prevProps.width !== this.props.width || prevProps.height !== this.props.height) {
       console.log('Resize', 'width', this.props.width, 'height', this.props.height);
@@ -90,6 +105,9 @@ export default class Viewer extends React.Component<Props, State> {
       this.controls.target.set(0, 0.75 * headY, 0);
 
       const data = this.state.data;
+      data.positionX = data.positionY = data.positionZ = 0;
+      data.rotationX = data.rotationY = data.rotationZ = 0;
+      data.scaleX = data.scaleY = data.scaleZ = 1;
       this.vrm.blendShapeMaster.blendShapeGroups.forEach(e => {
         data['blendShape' + e.name] = 0;
       });
@@ -99,6 +117,8 @@ export default class Viewer extends React.Component<Props, State> {
         this.mixer.stopAllAction();
       }
       this.mixer = new VRMAnimationMixer(this.vrm);
+
+      this.physics = new VRMPhysics(this.vrm);
 
       shouldUpdateAnimation = true;
     }
@@ -116,6 +136,36 @@ export default class Viewer extends React.Component<Props, State> {
       this.actions.forEach(e => {
         e.play();
       });
+    }
+
+    if (prevState.data.positionX !== this.state.data.positionX) {
+      this.vrm.model.position.setX(this.state.data.positionX);
+    }
+    if (prevState.data.positionY !== this.state.data.positionY) {
+      this.vrm.model.position.setY(this.state.data.positionY);
+    }
+    if (prevState.data.positionZ !== this.state.data.positionZ) {
+      this.vrm.model.position.setZ(this.state.data.positionZ);
+    }
+
+    if (
+      prevState.data.rotationX !== this.state.data.rotationX ||
+      prevState.data.rotationY !== this.state.data.rotationY ||
+      prevState.data.rotationZ !== this.state.data.rotationZ
+    ) {
+      this.vrm.model.setRotationFromEuler(
+        new THREE.Euler(this.state.data.rotationX, this.state.data.rotationY, this.state.data.rotationZ, 'YXZ')
+      );
+    }
+
+    if (prevState.data.scaleX !== this.state.data.scaleX) {
+      this.vrm.model.scale.setX(this.state.data.scaleX);
+    }
+    if (prevState.data.scaleY !== this.state.data.scaleY) {
+      this.vrm.model.scale.setY(this.state.data.scaleY);
+    }
+    if (prevState.data.scaleZ !== this.state.data.scaleZ) {
+      this.vrm.model.scale.setZ(this.state.data.scaleZ);
     }
 
     this.renderScene();
@@ -177,6 +227,18 @@ export default class Viewer extends React.Component<Props, State> {
             <DatBoolean path="isAxesVisible" label="Axes" />
             <DatNumber path={'timeScale'} label={'Time Scale'} min={0} max={2} step={0.01} />
           </DatFolder>
+          <DatFolder title="Transform" closed={true}>
+            <DatNumber path={'positionX'} label={'Position X'} min={-2.5} max={2.5} step={0.01} />
+            <DatNumber path={'positionY'} label={'Position Y'} min={-2.5} max={2.5} step={0.01} />
+            <DatNumber path={'positionZ'} label={'Position Z'} min={-2.5} max={2.5} step={0.01} />
+            <DatNumber path={'rotationX'} label={'Rotation X'} min={-Math.PI} max={Math.PI} step={0.01} />
+            <DatNumber path={'rotationY'} label={'Rotation Y'} min={-Math.PI} max={Math.PI} step={0.01} />
+            <DatNumber path={'rotationZ'} label={'Rotation Z'} min={-Math.PI} max={Math.PI} step={0.01} />
+            <DatNumber path={'scaleX'} label={'Scale X'} min={0.01} max={2} step={0.01} />
+            <DatNumber path={'scaleY'} label={'Scale Y'} min={0.01} max={2} step={0.01} />
+            <DatNumber path={'scaleZ'} label={'Scale Z'} min={0.01} max={2} step={0.01} />
+            <DatButton label={'Reset'} onClick={this.resetTransform} />
+          </DatFolder>
           <DatFolder title="Blend Shape Group" closed={true}>
             {this.vrm &&
               this.vrm.blendShapeMaster.blendShapeGroups.map((e, i) => (
@@ -221,6 +283,10 @@ export default class Viewer extends React.Component<Props, State> {
       this.mixer.update(this.state.data.timeScale * delta);
     }
 
+    if (this.physics) {
+      this.physics.update(this.state.data.timeScale * delta);
+    }
+
     this.renderScene();
   }
 
@@ -257,6 +323,23 @@ export default class Viewer extends React.Component<Props, State> {
 
     this.controls.update();
     this.renderer.render(this.scene, this.camera);
+  }
+
+  private resetTransform() {
+    this.setState({
+      data: {
+        ...this.state.data,
+        positionX: 0,
+        positionY: 0,
+        positionZ: 0,
+        rotationX: 0,
+        rotationY: 0,
+        rotationZ: 0,
+        scaleX: 1,
+        scaleY: 1,
+        scaleZ: 1,
+      },
+    });
   }
 
   private restartAnimation() {
