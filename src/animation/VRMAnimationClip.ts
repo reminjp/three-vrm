5947b2455d654b8f0bbbe9c5a9a73ec27658f600
rdrgn <noreply@remin.jp>
2019-03-20 02:53:09 +0900

    refactor VMD classes

--- src/animation/VRMAnimationClip.ts
+++
@@ -1,9 +0,0 @@
-import * as THREE from 'three';
-
-export class VRMAnimationClip {
-  public clips: Array<{ clip: THREE.AnimationClip; root: THREE.Object3D }>;
-
-  constructor() {
-    this.clips = [];
-  }
-}
